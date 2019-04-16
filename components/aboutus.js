c15cbb9656889cbe19863445831a1b81c6166328
Sydney Crumley <srsimps19@gmail.com>
2019-04-15 22:04:17 -0500

    added CSS and broken JS to about us page

--- components/aboutus.js
+++ components/aboutus.js
@@ -1,15 +1,32 @@
-class Team {
-    constructor(domElement);
-    this.domElement = domElement;
-    this.expandButton = this.domElement.querySelector('.expandButton');
-    this.expandButton.addEventListener('click', () => this.expandTeam());
+class TabLink {
+    constructor(element) {
+        this.element;
+        this.data = this.element.dataset.tab;
+        this.itemElement = document.querySelector(`.team[data-tab='${this.data}']`);
+        this.tabItem = new TabItem(this.itemElement);
+        this.element.addEventListener('click', () => this.select());
+    };
 
-    expandTeam() {
-        this.domElement.classList.toggle('team-open');
+    select() {
+        const tabs = document.querySelectorAll('.tab');
+        tabs.forEach(tab => {
+            tab.classList.remove('active-tab');
+        })
+        this.element.classList.add('active-tab');
+        this.tabItem.select();
     }
 }
 
-let teamMembers = document.querySelectorAll('.team');
-teamMembers.forEach(function(teamMember){
-    return new Team(teamMember);
-})
+class TabItem {
+    constructor(element) {
+        this.element = element;
+    }
+
+    select() {
+        const items = document.querySelectorAll(`.tabs-items`);
+        items.forEach(item => {
+            item.classList.remove('active-tab');
+        })
+        this.element.classList.add('active-tab')
+    }
+}
